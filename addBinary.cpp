7587a58ed0d3b44e7361a2bcfe33d23839f81a0b
pop0432 <jamesxia0432@gmail.com>
2015-12-08 21:53:09 -0800

    edit addbinary

--- addBinary.cpp
+++ addBinary.cpp
@@ -8,10 +8,21 @@ class Solution{
 public:
     string addBinary(string & a, string & b){
 
-      string result = "test";
+     string result;
+     int carry = 0;
 
+     for(int i = a.length()-1, j = b.length()-1; i>=0 || j >=0 ; --i, --j){
 
+        int num1 = i>= 0 ? a[i] - '0' : 0;
+        int num2 = j>= 0 ? b[j] - '0' : 0;
+        int val = (num1 + num2 + carry)/2;
+        carry = (num1 + num2 + carry)%2;
+        result.insert( result.begin(), val + '0');
 
+     }
+     if( carry == 1){
+        result.insert( result.begin(), '1');
+     }
 
       return result;
     }
