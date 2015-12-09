94be23eaa99ae2469501c9dbf82eaa4b94796456
pop0432 <jamesxia0432@gmail.com>
2015-12-08 21:43:41 -0800

    upload addBinary.cpp

---
+++ addBinary.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution{
+
+public:
+    string addBinary(string & a, string & b){
+
+      string result = "test";
+
+
+
+
+      return result;
+    }
+
+};
+
+int main()
+{
+  Solution test;
+  string a = "11";
+  string b = "10";
+
+  std::cout <<  test.addBinary(a, b)<< std::endl;
+
+
+
+}
